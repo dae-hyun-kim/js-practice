ed4f6bddc2697bdf9e4f6947f0ebf758f17485cc
dae-hyun-kim <daehk4@uci.edu>
2020-06-02 00:15:38 +0000

    prototyping and inheritance practice - Dae Kim

--- section-5/script.js
+++ section-5/script.js
@@ -19,6 +19,8 @@ Person.prototype.calculateAge = function() {
   console.log(2020 - this.yearOfBirth)
 }
 
+Person.prototype.lastName = "Smith";
+
 const john = new Person("John", 1990, "teacher");
 
 const jane = new Person("Jane", 1969, "designer");
@@ -28,3 +30,7 @@ const mark = new Person("Mark", 1948, "retired");
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
