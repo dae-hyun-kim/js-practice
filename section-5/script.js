4049305cf21215aed56ec249e61a151944eaeaee
dae-hyun-kim <daehk4@uci.edu>
2020-06-02 21:30:06 +0000

    using object create with second parameter for value

--- section-5/script.js
+++ section-5/script.js
@@ -51,3 +51,9 @@ const john = Object.create(personProto);
 john.name = "John";
 john.yearOfBirth = 1990;
 john.job = "teacher";
+
+const jane = Object.create(personProto, {
+  name: { value: "Jane"},
+  yearOfBirth: { value: 1969},
+  job: { value: "designer"}
+})
