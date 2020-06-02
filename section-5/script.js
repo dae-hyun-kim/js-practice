b8f4fd42e41e1c20e47f5b9aec2f7bfb560b2e87
dae-hyun-kim <daehk4@uci.edu>
2020-06-02 00:09:10 +0000

    created calculate age method - Dae Kim

--- section-5/script.js
+++ section-5/script.js
@@ -13,6 +13,9 @@ const Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
+  this.calculateAge = () => {
+    console.log(2020 - this.yearOfBirth)
+  }
 }
 
 const john = new Person("John", 1990, "teacher");
