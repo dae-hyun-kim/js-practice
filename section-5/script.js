542dca1f2e7f4dd0ff191265ea1630f17d574329
dae-hyun-kim <daehk4@uci.edu>
2020-06-02 00:14:08 +0000

    calculate prototype

--- section-5/script.js
+++ section-5/script.js
@@ -13,9 +13,18 @@ const Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = () => {
-    console.log(2020 - this.yearOfBirth)
-  }
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2020 - this.yearOfBirth)
 }
 
 const john = new Person("John", 1990, "teacher");
+
+const jane = new Person("Jane", 1969, "designer");
+
+const mark = new Person("Mark", 1948, "retired");
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
