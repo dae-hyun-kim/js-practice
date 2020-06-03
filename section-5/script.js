b111c144597bf5c87ddc04b8a52f1deaca6faf9b
dae-hyun-kim <daehk4@uci.edu>
2020-06-03 23:54:56 +0000

    completed expert level challenge

--- section-5/script.js
+++ section-5/script.js
@@ -396,6 +396,7 @@ askQuestions(myQuestionsArray)
 
 //Instructor Answer
 
+/*
 (function() {
   const Question = function (question, answers, correctAnswer) {
     this.question = question;
@@ -433,4 +434,89 @@ askQuestions(myQuestionsArray)
 
   myQuestionsArray[n].checkAnswer(answer);
 
+})();
+
+*/
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function () {
+  const Question = function (question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+  Question.prototype.askQuestion = function () {
+    console.log(this.question);
+
+    for (let i = 0; i < this.answers.length; i++) {
+      console.log(`${i}: ${this.answers[i]}`)
+    }
+  }
+
+  Question.prototype.checkAnswer = function (ans, callback) {
+    let sc;
+
+    if (ans === this.correctAnswer) {
+      console.log("That is the correct answer");
+      sc = callback(true);
+    } else {
+      console.log("That is the incorrect answer. Try Again.");
+      sc = callback(false);
+    }
+
+    this.displayScore(sc)
+  }
+
+  Question.prototype.displayScore = function(score) {
+    console.log(`Your current score is ${score}`);
+    console.log("----------------------")
+  }
+
+  const myNameQuestion = new Question("What is my name?", ["Danny", "Kevin", "David"], 0);
+  const myAgeQuestion = new Question("What is my age?", [24, 29, 32, 25], 3);
+  const myGenderQuestion = new Question("What is my gender?", ["female", "male"], 1);
+
+  const myQuestionsArray = [myNameQuestion, myAgeQuestion, myGenderQuestion];
+
+  function score() {
+    let sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++
+      }
+      return sc;
+    }
+  }
+
+  const keepScore = score();
+
+  function nextQuestion() {
+
+    let n = Math.floor(Math.random() * myQuestionsArray.length);
+
+    myQuestionsArray[n].askQuestion();
+
+    let answer = prompt("Please select the correct answer (input just the number)");
+
+    if (answer !== "exit") {
+
+      myQuestionsArray[n].checkAnswer(parseInt(answer), keepScore);
+
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
 })();
