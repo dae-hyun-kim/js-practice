4c5f59dfb440969baf76d62e58e03ede68059bf2
dae-hyun-kim <daehk4@uci.edu>
2020-06-03 18:09:14 +0000

    completed functions returning functions

--- section-5/script.js
+++ section-5/script.js
@@ -107,6 +107,7 @@ console.log(obj.city)
 
                         // Passing Functions as arguments
 
+/*
 const years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -143,3 +144,33 @@ const rates = arrayCalc(ages, heartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates)
+
+*/
+
+                      // Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return (name) => {
+      console.log(name + ", can you please explain what you UX design is?");
+    }
+  } else if  (job === "teacher") {
+    return (name) => {
+      console.log("What subject do you teach, " + name);
+    }
+  } else {
+    return (name) => {
+      console.log(`Hello. ${name}, what do you do?`);
+    }
+  }
+}
+
+const teacherQuestion = interviewQuestion("teacher");
+const designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+
+interviewQuestion("teacher")("Mark");
