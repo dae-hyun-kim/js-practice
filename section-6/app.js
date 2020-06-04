174ca09f70ed9274f4757f985e5e098b15fc6d24
dae-hyun-kim <daehk4@uci.edu>
2020-06-04 18:57:15 +0000

    added event listener to add item button (keypress and click)

--- section-6/app.js
+++ section-6/app.js
@@ -1,34 +1,52 @@
+// BUDGET CONTROLLER
 const budgetController = (function() {
 
-  const x = 23;
+  // some code
 
-  const add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return (add(b))
-    }
-  }
 })();
 
 
+
+
+// UI CONTROLLER
 
 const UIController = (function() {
 
+  // some code
 
 })();
 
 
+
+// GLOBAL CONTROLLER
+
 const controller = (function(budgetCtrl, UICtrl) {
 
-  const z = budgetCtrl.publicTest(5);
+  const ctrlAddItem = function() {
 
-  return {
-    anotherPublic: function() {
-      console.log(z)
-    }
+
+    console.log("hello")
+     // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the user interface
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on user interface
+
   }
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem)
+
+  document.addEventListener("keypress", (event) => {
+
+    if (event.keyCode === 13 || event.which === 13) {
+
+      ctrlAddItem();
+
+    }
+  })
 
 })(budgetController, UIController);
