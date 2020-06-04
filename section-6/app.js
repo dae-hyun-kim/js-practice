1a3a6e18328e3d6c34cb79bbc38016a45d76943f
dae-hyun-kim <daehk4@uci.edu>
2020-06-04 18:40:48 +0000

    completed modular structuring of code

---
+++ section-6/app.js
@@ -0,0 +1,34 @@
+const budgetController = (function() {
+
+  const x = 23;
+
+  const add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return (add(b))
+    }
+  }
+})();
+
+
+
+const UIController = (function() {
+
+
+})();
+
+
+const controller = (function(budgetCtrl, UICtrl) {
+
+  const z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z)
+    }
+  }
+
+})(budgetController, UIController);
