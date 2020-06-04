39d19d92d05412fd06aa71926b0a6607bfee5f2c
dae-hyun-kim <daehk4@uci.edu>
2020-06-04 20:27:05 +0000

    able to read input data

--- section-6/app.js
+++ section-6/app.js
@@ -12,7 +12,27 @@ const budgetController = (function() {
 
 const UIController = (function() {
 
-  // some code
+  const DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addButton: ".add__btn",
+  }
+
+  return {
+
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
 
 })();
 
@@ -22,11 +42,14 @@ const UIController = (function() {
 
 const controller = (function(budgetCtrl, UICtrl) {
 
+  const DOM = UICtrl.getDOMstrings();
+
   const ctrlAddItem = function() {
 
+    // 1. Get the field input data
 
-    console.log("hello")
-     // 1. Get the field input data
+    const input = UICtrl.getInput();
+    console.log(input)
 
     // 2. Add the item to the budget controller
 
@@ -38,15 +61,15 @@ const controller = (function(budgetCtrl, UICtrl) {
 
   }
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem)
+  document.querySelector(DOM.addButton).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", (event) => {
 
     if (event.keyCode === 13 || event.which === 13) {
-
       ctrlAddItem();
-
     }
+
+
   })
 
 })(budgetController, UIController);
