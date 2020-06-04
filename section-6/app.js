18402b8cc2ea4a547e2fbc7ce6d57e70b6a27466
dae-hyun-kim <daehk4@uci.edu>
2020-06-04 20:59:58 +0000

    created function constructors for Income and Expense

--- section-6/app.js
+++ section-6/app.js
@@ -1,7 +1,28 @@
 // BUDGET CONTROLLER
 const budgetController = (function() {
 
-  // some code
+  const Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    }
+  }
 
 })();
 
