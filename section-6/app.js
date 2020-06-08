3dae4f868f0a63ce949fc8906532e68177fe4e7c
dae-hyun-kim <daehk4@uci.edu>
2020-06-08 22:06:37 +0000

    calculate percentage on budget controller

--- section-6/app.js
+++ section-6/app.js
@@ -5,7 +5,20 @@ const budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1
   };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  }
 
   const Income = function (id, description, value) {
     this.id = id;
@@ -92,6 +105,22 @@ const budgetController = (function() {
       } else {
         data.percentage = -1
       }
+    },
+
+    calculatePercentages: function() {
+
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      })
+
+    },
+
+    getPercentages: function() {
+
+      const allPerc = data.allItems.exp.map(function(cur) {
+        return cur.getPercentage();
+      })
+      return allPerc;
     },
 
     getBudget: function() {
@@ -241,9 +270,15 @@ const controller = (function(budgetCtrl, UICtrl) {
 
     // 1. Calculate percentages
 
+    budgetCtrl.calculatePercentages();
+
     // 2. Read percentages from the budget controller
 
+    const percentages = budgetCtrl.getPercentages();
+
     // 3. Update the UI with the new percentages
+
+    console.log(percentages)
 
   };
 
