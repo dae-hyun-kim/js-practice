528f3015d6101a4f1daa07501bdec4a476d3d48a
dae-hyun-kim <daehk4@uci.edu>
2020-06-09 21:32:31 +0000

    maps practice

--- section-7/script.js
+++ section-7/script.js
@@ -422,3 +422,35 @@ var john = new SmithPerson("John", 1990);
 var emily = new SmithPerson("Emily", 1983, "Diaz", "Spanish");
 
 */
+
+/*
+// LECTURE: MAPS
+
+const question = new Map();
+question.set("question", "What is the official name of the latest major JavaScript version?");
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct Answer");
+question.set(false, "Wrong, Please try again!");
+
+console.log(question.get("question"));
+console.log(question.size);
+
+if (question.has(4)) {
+  console.log("Answer 4 is here")
+}
+
+// question.forEach((value, key) => {
+//   console.log(`This is the ${key}, and it's set to ${value}`)
+// })
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === "number") {
+    console.log(`Answer ${key}: ${value}`)
+  }
+}
+
+*/
