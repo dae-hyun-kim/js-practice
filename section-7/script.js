cbd5097cfc5b3b2d8a2b1f74fd4a447b45e38acc
dae-hyun-kim <daehk4@uci.edu>
2020-06-09 03:09:34 +0000

    Blocks vs IIFE

--- section-7/script.js
+++ section-7/script.js
@@ -58,3 +58,25 @@ for (let i = 0; i < 5; i++) {
 console.log(i)
 
 */
+
+// LECTURE: BLOCKS AND IFFE
+
+/*
+
+// ES5
+(function() {
+  var c = 3;
+})();
+
+
+// ES6
+
+{
+  const a = 1;
+  let b = 2;
+  var c = 3
+}
+
+console.log(c)
+
+*/
