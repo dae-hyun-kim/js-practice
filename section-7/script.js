a40462679741cacdcaf5f1a0615fae117164eb82
dae-hyun-kim <daehk4@uci.edu>
2020-06-09 03:32:23 +0000

    template literal and string methods (startsWith, endsWith, includes, repeat)

--- section-7/script.js
+++ section-7/script.js
@@ -80,3 +80,27 @@ console.log(i)
 console.log(c)
 
 */
+
+
+// LECTURE: STRINGS
+
+let firstName = "John";
+let lastName = "Smith";
+const yearOfBirth = 1990;
+function calcAge(year) {
+  return 2016 - year;
+}
+
+// ES5
+console.log("This is " + firstName + " " + lastName+ ". He was born in " + yearOfBirth + ". Today, he is " + calcAge(yearOfBirth) + " years old.");
+
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`)
+
+const n = `${firstName} ${lastName}`;
+
+console.log(n.startsWith("J"));
+console.log(n.endsWith("h"));
+console.log(n.includes(" "));
+console.log(`${firstName} `.repeat(5))
