21a937356b976170b300197d5e6c2ac8fb8d8d37
dae-hyun-kim <daehk4@uci.edu>
2020-06-09 18:55:29 +0000

    arrow function with bind

--- section-7/script.js
+++ section-7/script.js
@@ -108,6 +108,7 @@ console.log(`${firstName} `.repeat(5))
 
 */
 
+/*
 // LECTURE: ARROW FUNCTIONS
 
 const years = [1990, 1965, 1982, 1937];
@@ -137,3 +138,84 @@ ages6 = years.map((el, index) => {
 })
 
 console.log(ages6)
+
+*/
+
+/*
+
+// LECTURE: ARROW FUNCTIONS 2
+
+// ES5
+var box5 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+
+    var self = this;
+
+    document.querySelector(".green").addEventListener("click", function() {
+      var str = "This is box number " + self.position + " and it is " + self.color;
+      alert(str)
+    })
+  }
+}
+
+// ES6
+
+const box6 = {
+  color: "green",
+  position: 1,
+  clickMe: function () {
+
+
+    document.querySelector(".green").addEventListener("click", () => {
+      const str = `This is box number ${this.position} and it is ${this.color}`
+      alert(str)
+    })
+  }
+}
+
+
+const box66 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+
+
+    document.querySelector(".green").addEventListener("click", () => {
+      const str = `This is box number ${this.position} and it is ${this.color}`
+      alert(str)
+    })
+  }
+}
+box66.clickMe();
+
+*/
+
+function Person(name) {
+  this.name = name;
+}
+
+// ES5
+
+Person.prototype.myFriends5 = function(friends) {
+
+  var arr = friends.map(function(el) {
+    return this.name + " is friends with " + el;
+  }.bind(this))
+  console.log(arr)
+}
+
+
+// ES6
+
+Person.prototype.myFriends6 = function (friends) {
+
+  const arr = friends.map((el) =>  {
+    return `${this.name} is friends with ${el}`;
+  })
+  console.log(arr)
+}
+
+const friends = ["Bob", "Jane", "Mark"];
+new Person("Dae").myFriends6(friends);
