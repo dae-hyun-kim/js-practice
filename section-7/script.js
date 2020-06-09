ccbe02065c65c6e184cc353daa4ecf81d7dd99a1
dae-hyun-kim <daehk4@uci.edu>
2020-06-09 19:17:12 +0000

    array from method for node lists

--- section-7/script.js
+++ section-7/script.js
@@ -224,6 +224,8 @@ new Person("Dae").myFriends6(friends);
 
 */
 
+
+/*
 // LECTURE: DESTRUCTURING
 
 // ES5
@@ -260,3 +262,23 @@ function calcAgeRetirement(year) {
 const [age2, retirement] = calcAgeRetirement(1990);
 console.log(age2);
 console.log(retirement)
+
+*/
+
+
+// LECTURE: ARRAYS
+
+const boxes = document.querySelectorAll(".box");
+
+// ES5
+/*
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = "dodgerblue"
+})
+*/
+
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach((cur) => {
+  cur.style.backgroundColor = "dodgerblue"
+})
