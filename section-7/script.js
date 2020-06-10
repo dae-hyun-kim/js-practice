0b50c4010b61dacead7e77d36b0f10d20c8cf8c2
dae-hyun-kim <daehk4@uci.edu>
2020-06-10 17:35:13 +0000

    classes introduction

--- section-7/script.js
+++ section-7/script.js
@@ -454,3 +454,44 @@ for (let [key, value] of question.entries()) {
 }
 
 */
+
+
+/*
+// LECTURE: CLASSES
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth;
+  console.log(age)
+}
+
+var john5 = new Person5("John", 1990, "Teacher");
+
+// ES6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job
+  }
+
+  calculateAge() {
+    const age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log("Hey there!")
+  }
+}
+
+const john6 = new Person6("John", 1990, "teacher");
+Person6.greeting();
+
+*/
